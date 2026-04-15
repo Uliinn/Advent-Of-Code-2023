1d405702c4f24e2b2d465e0b98a0c9939ebec348
Uliinn <isakul@student.chalmers.se>
2026-04-16 00:24:38 +0200

    Much better day 6 part 2

--- day6/day6_part2.cpp
+++ day6/day6_part2.cpp
@@ -1,15 +1,19 @@
 #include "../common.h"
+#include <math.h>
 
 
 int main(int argc, char **argv) {
+  
+  
 
+  Timer timer;
+  timer.begin();
+  
   int pow10[10] = {
         1, 10, 100, 1000, 10000, 
         100000, 1000000, 10000000, 100000000, 1000000000
     };
 
-  Timer timer;
-  
   std::ifstream file("../day6/day6_input.txt");
 
   std::vector<std::string> lines = readlines(file);
@@ -31,18 +35,23 @@ int main(int argc, char **argv) {
     real_distance *= pow10[len];
     real_distance += parse_ll(dist);
   }
-  timer.begin();
+  
 
-  int possibilities = 0;
+  // 2 roots, and in between them.
+  // Old loop had (time - s) * s > distance -> time*s - s^2 > distance which it is between 2 roots
+  // s^2 - time*s + distance = 0
+  // s = time/2 +- sqrt(time^2/4 - distance)
+  // root1 = time/2 - sqrt(time^2/4 - distance)
+  // root2 = time/2 + sqrt(time^2/4 - distance)
+  // All in betweeen is root2 - root1
+  // root1 = A - B
+  // root2 = A + B
+  // Then interval is A + B - (A - B) = 2B
+  // If it was A = 1 and B = 1 then the range would be from [0,2] so 3 points, 2B is only 2, then + 1 for offset
 
-    for (int s = 1; s < real_time; s++) {
+  long long B = std::sqrt((real_time * real_time) / 4 - real_distance);
 
-      if ((real_time - s) * s > real_distance ) {
-        possibilities++;
-      }
-  }
-   
-  std::cout << possibilities << '\n';
+  std::cout << 2 * B + 1 << '\n';
   timer.stop();
   return 0;
   }

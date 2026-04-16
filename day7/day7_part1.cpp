6082e69b7a83f00b82f325d9c1f18166094fff7b
Uliinn <isakul@student.chalmers.se>
2026-04-16 09:54:24 +0200

    Day 7 part 1

--- day7/day7_part1.cpp
+++ day7/day7_part1.cpp
@@ -1,13 +1,10 @@
-
-
-
-// 1.Sort into different vectors (one pair, two pair etc.) in pairs of string_view and int
-// 2.For every vector, starting from one pair and up to Five of a kind, sort them from best to worst. rank = 1
-// iterate over them and add the bid times rank to sum (long long) and rank++.
-
 #include "../common.h"
-#include <math.h>
+#include <algorithm>
 
+struct Player {
+  std::string_view card;
+  int bid;
+};
 
 int symbol_to_int(const char symbol) {
   switch (symbol)
@@ -33,7 +30,91 @@ int symbol_to_int(const char symbol) {
   }
 }
 
-int main(int argc, char **argv) {
+bool sortingFunc(Player a, Player b) {
+
+  for (int i = 0; i < 5; i++) {
+    int as = symbol_to_int(a.card[i]);
+    int bs = symbol_to_int(b.card[i]);
+    if (as == bs)
+      continue;
+    return as < bs;
+  }
+  return true;
+}
+
+
+
+std::vector<int> count(std::string_view s) {
+  std::vector<int> result;
+  std::vector<char> seen;
   
+  for (char c : s) {
+    auto i = std::find(seen.begin(), seen.end(), c);
+    if (i != seen.end()) { // in
+      result[i - seen.begin()]++;
+    } else {
+      seen.push_back(c);
+      result.push_back(1);
+    }
+  }
+  std::sort(result.begin(), result.end(), [](int a, int b)
+            { return a > b; });
+
+  return result;
+}
+
+
+int main(int argc, char **argv) {
+
+  Timer timer;
+  timer.begin();
+  std::ifstream file("../day7/day7_input.txt");
+  std::vector<std::string> lines = readlines(file);
+
+  std::vector<Player> high, one, two, three, full, four, five;
+
+  for (const std::string& s :lines) {
+    std::vector<std::string_view> line = split(s);
+    Player player = {
+        line[0],
+        parse_int(line[1])
+    };
+    std::vector<int> c = count(line[0]);
+    
+    if (c[0] == 5) {
+      five.push_back(player);
+    } else if (c[0] == 4) {
+      four.push_back(player);
+    } else if (c[0] == 3 && c[1] == 2) {
+      full.push_back(player);
+    } else if (c[0] == 3) {
+      three.push_back(player);
+    } else if (c[0] == 2 && c[1] == 2) {
+      two.push_back(player);
+    } else if (c[0] == 2) {
+      one.push_back(player); 
+    } else {
+      high.push_back(player);
+    }
+  }
+
+  long long sum = 0;
+
+  int rank = 1;
+
+
+  std::vector<std::vector<Player>> all = {high, one, two, three, full, four, five};
+
+  for (std::vector<Player>& v : all) {
+    std::sort(v.begin(), v.end(), sortingFunc);
+
+    for (Player p : v) {
+      sum += p.bid * rank;
+      rank += 1;
+    }
+  }
+
+  std::cout << sum << '\n';
+  timer.stop();
   return 0;
 }
